5a8f8634ad6a6c9e00a23f664c935d3657c2d04c
Felipe Mathais <felipe.magnossao@midway.com.br>
2021-04-03 10:12:02 -0300

    Created error handler to format request validation.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import {json} from 'body-parser';
 
 import { SignupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(json());
 
 app.use(SignupRouter);
 
+app.use(errorHandler);
+
 app.listen(3000, () => {
-    console.log('Listing on port 3000!');
+    console.log('Listing on port 3000!!!');
 });
