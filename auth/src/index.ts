c4538a6a5876770878c3e59638f16360c1a4e079
Felipe Mathais <felipe.magnossao@midway.com.br>
2021-04-03 10:05:53 -0300

    Created endpoint for user registration.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import {json} from 'body-parser';
 
+import { SignupRouter } from './routes/signup';
+
 const app = express();
 app.use(json());
 
-app.get('/', (req, res) => {
-    res.send('It is working!');
-});
+app.use(SignupRouter);
 
 app.listen(3000, () => {
     console.log('Listing on port 3000!');
