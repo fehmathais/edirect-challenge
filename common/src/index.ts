7da2df4161516a2b711849a398820b84461ce65e
Felipe Mathais <felipe.magnossao@midway.com.br>
2021-04-03 14:11:16 -0300

    updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -4,5 +4,6 @@ export * from "./errors/bad-request-error";
 export * from "./errors/request-validation-error";
 export * from "./errors/database-connection-error";
 
+export * from "./middlewares/current-user";
 export * from "./middlewares/error-handler";
 export * from "./middlewares/validate-request";
