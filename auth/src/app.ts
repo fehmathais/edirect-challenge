63af04fad76fac9c975a75dfc8bec7b363b800da
Felipe Mathais <felipe.magnossao@midway.com.br>
2021-04-03 13:21:57 -0300

    Created user signin endpoint.

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import 'express-async-errors';
 import {json} from 'body-parser';
-import cookieSession from "cookie-session";
+import cookieSession from 'cookie-session';
 
 import { SignupRouter } from './routes/signup';
+import { SigninRouter } from './routes/signin';
 import { NotFoundError } from '@fm-challenge/common';
 import { errorHandler } from '@fm-challenge/common';
 
@@ -16,6 +17,7 @@ app.use(cookieSession({
 }));
 
 app.use(SignupRouter);
+app.use(SigninRouter);
 
 app.all('*', async () => {
     throw new NotFoundError();
--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -9,4 +9,10 @@ export class Password {
         const buffer = (await scryptAsync(password, salt, 64)) as Buffer;
         return `${buffer.toString('hex')}.${salt}`;
     }
+
+    static async compare(storedPassword: string, suppliedPassword: string) {
+        const [hashedPassword, salt] = storedPassword.split('.');
+        const buffer = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+        return buffer.toString('hex') === hashedPassword;
+    }
 }
