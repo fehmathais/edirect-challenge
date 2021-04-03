af6fc0de411f373f3d8eb10e33ba573665e00ced
Felipe Mathais <felipe.magnossao@midway.com.br>
2021-04-03 10:29:08 -0300

    Created error handler for not found endpoints.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,19 @@
 import express from 'express';
+import 'express-async-errors';
 import {json} from 'body-parser';
 
 import { SignupRouter } from './routes/signup';
+import { NotFoundError } from './errors/not-found-error';
 import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(json());
 
 app.use(SignupRouter);
+
+app.all('*', async () => {
+    throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
