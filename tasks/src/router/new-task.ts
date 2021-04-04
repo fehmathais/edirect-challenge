6a0659c7a6f45a4be0893ab216029d455e0423f7
Felipe Mathais <felipe.magnossao@midway.com.br>
2021-04-04 15:01:15 -0300

    Created new task router.

--- tasks/src/router/new-task.ts
+++ tasks/src/router/new-task.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from 'express';
 import moment from 'moment';
-import {body} from 'express-validator';
-import { requireAuth, validateRequest } from '@fm-challenge/common';
+import mongoose from "mongoose";
+import { body } from 'express-validator';
+import { Task } from "../models/task";
+import { Project } from "../models/project";
+import { BadRequestError, NotFoundError, requireAuth, validateRequest } from '@fm-challenge/common';
+import { TaskStatus } from "../models/task-status";
 
 const router = express.Router();
 
@@ -16,11 +20,37 @@ router.post(
         body('expiration')
             .custom(value => moment(value).isValid())
             .withMessage('Expiration must be a valid date!'),
+        body('projectId')
+            .not()
+            .isEmpty()
+            .custom(value => mongoose.Types.ObjectId.isValid(value))
+            .withMessage('You must provide a valid project reference!'),
     ],
     validateRequest,
     async (req: Request, res: Response) => {
-        const {description, expiration} = req.body;
-        res.status(201).send({description, expiration});
+        const {description, expiration, projectId} = req.body;
+        
+        const project = await Project.findById(projectId);
+        
+        if (!project) {
+            throw new NotFoundError();
+        }
+        
+        const currentDate = moment().format();
+        
+        if (expiration <= currentDate) {
+            throw new BadRequestError('You must provide a valid expiration date!');
+        }
+        
+        const task = Task.build({
+            description,
+            expiration,
+            projectId,
+            status: TaskStatus.Created
+        });
+        
+        await task.save();
+        res.status(201).send(task);
     });
 
 export { router as NewTaskRouter };
