72ce46572cb12c0a8c7f9386fd3fcc10d275b135
Felipe Mathais <felipe.magnossao@midway.com.br>
2021-04-04 17:05:15 -0300

    updates

--- common/src/events/created-task-event.ts
+++ common/src/events/created-task-event.ts
@@ -3,9 +3,9 @@ import {Subjects} from "./subjects";
 export interface CreatedTaskEvent {
     subject: Subjects.TaskCreated;
     data: {
-        userId: string;
-        email: string;
-        type: string;
-        verificationCode: string;
+        description: string;
+        expiration: Date;
+        status: string;
+        projectId: string;
     }
 }
